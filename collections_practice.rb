3e5f770eed792447a72229b0f73123b0c3a5f884
Daniel Lechok <dclechok@gmail.com>
2019-09-12 13:39:11 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,7 +41,7 @@ end
 
 
 def add_s(array)
-      array.each_with_index.collect do |word,index|
+      array.each_with_index.collect do |word, index|
         if index != 1 
           word = word + "s"
         else
